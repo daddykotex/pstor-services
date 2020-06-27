515f255011658f8d55040a4dd3ab8fde4afef277
David Francoeur <dfrancoeur04@gmail.com>
2020-06-27 14:28:51 -0400

    Work directory option, with check

---
+++ index-builder/src/main/scala/Helper.scala
@@ -0,0 +1,18 @@
+import java.nio.file.Files
+import java.nio.file.Path
+import cats.effect.Blocker
+import cats.effect.Sync
+import cats.implicits._
+import cats.effect.ContextShift
+import cats.NonEmptyParallel
+
+object Helper {
+  private def existsCheck[F[_]: Sync](path: Path): F[Boolean] = Sync[F].delay(Files.exists(path))
+  private def directoryCheck[F[_]: Sync](path: Path): F[Boolean] = Sync[F].delay(Files.isDirectory(path))
+  private def writableCheck[F[_]: Sync](path: Path): F[Boolean] = Sync[F].delay(Files.isWritable(path))
+
+  def checkPath[F[_]: NonEmptyParallel: Sync: ContextShift](path: Path, blocker: Blocker): F[Boolean] = {
+    val allChecks: F[Boolean] = (existsCheck(path), directoryCheck(path), writableCheck(path)).parMapN(_ && _ && _)
+    blocker.blockOn(allChecks)
+  }
+}
--- index-builder/src/main/scala/Main.scala
+++ index-builder/src/main/scala/Main.scala
@@ -6,6 +6,8 @@ import cats.effect.{ExitCode, IO}
 import b2.B2Credentials
 import b2.B2Client
 import b2.models.BucketResponse
+import java.nio.file.Path
+import cats.effect.Blocker
 
 object Main
     extends CommandIOApp(
@@ -13,36 +15,46 @@ object Main
       header = "Build an index from a B2 bucket."
     ) {
   override def main: Opts[IO[ExitCode]] = {
-    val bucketName =
+    val b2BucketNameOpts =
       Opts.option[String](
         "bucket-name",
         help = "The bucket name. The credentials you will use need to be able to download files from this bucket"
       )
 
-    val b2AppKeyId =
+    val b2AppKeyIdOpts =
       Opts.option[String]("key-id", help = "A B2 Cloud Key ID")
-    val b2AppKey =
+    val b2AppKeyOpts =
       Opts.option[String]("key", help = "A B2 Cloud Key")
 
-    (bucketName, b2AppKeyId, b2AppKey).mapN {
-      case (_, keyId, key) =>
-        val credentials = B2Credentials(keyId, key)
-        val clientProgram = for {
-          token <- B2Client.getToken[IO](credentials)
-          bucketStr <- B2Client.listBuckets[IO](token)
-        } yield bucketStr
+    val workDirOpts =
+      Opts.option[Path]("work-dir", help = "The directory where the index will be built.")
 
-        val tokenIO =
-          B2Client.run[IO, BucketResponse](clientProgram)
-        tokenIO
-          .flatTap(bucketResponse =>
-            IO.delay {
+    (b2BucketNameOpts, b2AppKeyIdOpts, b2AppKeyOpts, workDirOpts).mapN {
+      case (_, keyId, key, workDir) =>
+        def program(blocker: Blocker): IO[ExitCode] = {
+          val credentials = B2Credentials(keyId, key)
+          val clientProgram = for {
+            token <- B2Client.getToken[IO](credentials)
+            bucketStr <- B2Client.listBuckets[IO](token)
+          } yield bucketStr
+
+          for {
+            workDirCheck <- Helper.checkPath[IO](workDir, blocker)
+            bucketResponse <- B2Client.run[IO, BucketResponse](clientProgram)
+            _ <- IO.delay {
               bucketResponse.buckets.foreach { b =>
-                print(s"Bucket name: ${b.bucketName}, bucket ID: ${b.bucketId}")
+                println(s"Bucket name: ${b.bucketName}, bucket ID: ${b.bucketId}")
+              }
+              if (workDirCheck) {
+                println(s"Proceeding with ${workDir.toAbsolutePath().toString()}")
+              } else {
+                println(s"Can't proceed with ${workDir.toAbsolutePath().toString()}")
               }
             }
-          )
-          .as(ExitCode.Success)
+          } yield ExitCode.Success
+        }
+
+        Blocker[IO].use(program(_))
     }
   }
 }
